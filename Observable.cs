811f5917d081e22f6a9bb04f1127740a91d331ed
Ferdinand-vW <ferdinandvwalree@gmail.com>
2015-04-18 16:06:52 +0200

    Added basis for architecture

---
+++ Observable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetM.Presenter;
+
+namespace NetM
+{
+    abstract class Observable
+    {
+        public delegate void Update();
+        public event Update onUpdate = null;
+
+        public void Subscribe(Observer observer)
+        {
+            onUpdate += new Update(observer.update);
+        }
+
+        public void Subscribe(Observer observer)
+        {
+            onUpdate -= new Update(observer.update);
+        }
+
+    }
+}
